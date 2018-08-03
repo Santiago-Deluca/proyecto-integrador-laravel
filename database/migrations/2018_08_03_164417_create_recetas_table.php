fbe73fd8904e69b656aee0ecd687b226bf771b29
Santiago Deluca <santiagodeluca07@gmail.com>
2018-08-03 13:57:37 -0300

    migracion tablas recetas y tipo

---
+++ database/migrations/2018_08_03_164417_create_recetas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecetasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recetas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_usuario');
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->integer('tipo');
+            $table->boolean('apto_celiacos');
+            $table->string('foto_comida');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recetas');
+    }
+}
