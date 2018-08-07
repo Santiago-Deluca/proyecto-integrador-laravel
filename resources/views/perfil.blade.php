4272023f85cdc8735d5841162871eec5b1e26756
juanaorlandini <38474414+juanaorlandini@users.noreply.github.com>
2018-08-07 16:08:45 -0300

    algun cambio en perfil

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
     <section class="cuerpo">
-      <div class="info_usuario">
-        <div class="imagen_perfil">
+      <div class="info-usuario">
+        <div class="imagen-perfil">
         	  <img src="./images/avatar/{{Auth::user()->avatar}}" alt="avatar" width=200px height=200px  style=border-radius:50% >
         </div>
         <p>{{Auth::user()->name}}</p>
-        <p>{{Auth::user()->email}}</p>
+        <p class="email-usuario">{{Auth::user()->email}}</p>
       </div>
 
       <div class="container">
 
         @foreach (\App\Receta::all() as $receta)
-        <div class="posteos">          
+        <div class="posteos">
           <h3>{{$receta->nombre}}</h3>
-          <img class="imagen_posteo"src="" alt="">
+          <img class="imagen_posteo"src="" alt="imagen_posteo">
           <p>Tipo de receta: {{$receta->tipo}}</p>
           <textarea name="descripcion" rows="8" cols="80" >{{$receta->descripcion}}</textarea>
           <a href="\app\Http\Controllers\RecetasController">Eliminar publicacion</a>
